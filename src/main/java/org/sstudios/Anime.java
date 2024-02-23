9f7512946876beb1386382b9df15f59d23c68cf6
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-23 16:58:47 -0600

    Add files via upload

---
+++ src/main/java/org/sstudios/Anime.java
@@ -0,0 +1,45 @@
+package org.sstudios;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class Anime {
+
+    private static final String animeImageUrl = "https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img-anime/";
+    private static final Random random = new Random();
+
+    public static String[] getRandomAnimeImageUrl() {
+        int randomNumber = random.nextInt(120) + 1; // Assuming there are 1000 images
+        String imageName = "img-anime" + randomNumber + ".jpg";
+        return new String[] { animeImageUrl + imageName + "?raw=true", imageName };
+    }
+
+    public static void downloadRandomAnimeImage() {
+        String[] imageInfo = getRandomAnimeImageUrl();
+        String animeImageUrlFull = imageInfo[0];
+        String imageName = imageInfo[1];
+        String directory = "./anime-images";
+
+        try {
+            if (!Files.exists(Paths.get(directory))) {
+                Files.createDirectories(Paths.get(directory));
+            }
+
+            URL url = new URL(animeImageUrlFull);
+            try (FileOutputStream fos = new FileOutputStream(directory + "/" + imageName)) {
+                fos.write(url.openStream().readAllBytes());
+                System.out.println("org.sstudios.Anime image " + imageName + " downloaded");
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to download image " + imageName + ": " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        downloadRandomAnimeImage();
+    }
+}
