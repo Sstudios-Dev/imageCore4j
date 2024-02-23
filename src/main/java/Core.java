702307d91cb25e861a88d56f83100e0f4301cd6b
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-02-23 16:22:01 -0600

    Add files via upload

---
+++ src/main/java/Core.java
@@ -0,0 +1,44 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class Core {
+
+    private static final String imageUrl = "https://raw.githubusercontent.com/Sstudios-Dev/image-core/main/src/img/";
+    private static final Random random = new Random();
+
+    public static String[] getImageUrlFull() {
+        int randomNumber = random.nextInt(46) + 1; // Assuming there are 1000 images
+        String imageName = "img-core" + randomNumber + ".jpg";
+        return new String[] { imageUrl + imageName + "?raw=true", imageName };
+    }
+
+    public static void downloadRandomImage() {
+        String[] imageInfo = getImageUrlFull();
+        String imageUrlFull = imageInfo[0];
+        String imageName = imageInfo[1];
+        String directory = "./images-Random";
+
+        try {
+            if (!Files.exists(Paths.get(directory))) {
+                Files.createDirectories(Paths.get(directory));
+            }
+
+            URL url = new URL(imageUrlFull);
+            try (FileOutputStream fos = new FileOutputStream(directory + "/" + imageName)) {
+                fos.write(url.openStream().readAllBytes());
+                System.out.println("Image random " + imageName + " downloaded");
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to download image " + imageName + ": " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        downloadRandomImage();
+    }
+}
